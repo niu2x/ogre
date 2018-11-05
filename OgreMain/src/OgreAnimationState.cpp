niu2x/ogre

commit 02beb5d4ff16db423b0a9a19227df8a8f1c92d91

Main: properly use std:: math overloads instead of casting to Real

OgreMain/src/OgreAnimationState.cpp
@@ -79,17 +79,12 @@ namespace Ogre
             if (mLoop)
             {
                 // Wrap
-                mTimePos = fmod(mTimePos, mLength);
-                if(mTimePos < 0)
-                    mTimePos += mLength;     
+                mTimePos = std::fmod(mTimePos, mLength);
             }
             else
             {
                 // Clamp
-                if(mTimePos < 0)
-                    mTimePos = 0;
-                else if (mTimePos > mLength)
-                    mTimePos = mLength;
+                mTimePos = Math::Clamp(mTimePos, Real(0), mLength);
             }
 
             if (mEnabled)

OgreMain/src/OgrePredefinedControllers.cpp
@@ -298,11 +298,7 @@ namespace Ogre
         Real input = getAdjustedInput(source * mFrequency);
         Real output = 0;
         // For simplicity, factor input down to {0,1)
-        // Use looped subtract rather than divide / round
-        while (input >= 1.0)
-            input -= 1.0;
-        while (input < 0.0)
-            input += 1.0;
+        input = std::fmod(input, Real(1));
 
         // Calculate output in -1..1 range
         switch (mWaveType)

OgreMain/include/OgreMath.h
@@ -247,8 +247,8 @@ namespace Ogre
         ~Math();
 
         static inline int IAbs (int iValue) { return ( iValue >= 0 ? iValue : -iValue ); }
-        static inline int ICeil (float fValue) { return int(ceil(fValue)); }
-        static inline int IFloor (float fValue) { return int(floor(fValue)); }
+        static inline int ICeil (float fValue) { return int(std::ceil(fValue)); }
+        static inline int IFloor (float fValue) { return int(std::floor(fValue)); }
         static int ISign (int iValue) {
             return ( iValue > 0 ? +1 : ( iValue < 0 ? -1 : 0 ) );
         }
@@ -257,19 +257,19 @@ namespace Ogre
             @param
                 fValue The value whose absolute value will be returned.
         */
-        static inline Real Abs (Real fValue) { return Real(fabs(fValue)); }
+        static inline Real Abs (Real fValue) { return std::abs(fValue); }
 
         /** Absolute value function
             @param dValue
                 The value, in degrees, whose absolute value will be returned.
          */
-        static inline Degree Abs (const Degree& dValue) { return Degree(fabs(dValue.valueDegrees())); }
+        static inline Degree Abs (const Degree& dValue) { return Degree(std::abs(dValue.valueDegrees())); }
 
         /** Absolute value function
             @param rValue
                 The value, in radians, whose absolute value will be returned.
          */
-        static inline Radian Abs (const Radian& rValue) { return Radian(fabs(rValue.valueRadians())); }
+        static inline Radian Abs (const Radian& rValue) { return Radian(std::abs(rValue.valueRadians())); }
 
         /** Arc cosine function
             @param fValue
@@ -287,7 +287,7 @@ namespace Ogre
             @param fValue
                 The value whose arc tangent will be returned.
          */
-        static inline Radian ATan (Real fValue) { return Radian(atan(fValue)); }
+        static inline Radian ATan (Real fValue) { return Radian(std::atan(fValue)); }
 
         /** Arc tangent between two values function
             @param fY
@@ -295,7 +295,7 @@ namespace Ogre
             @param fX
                 The second value to calculate the arc tangent with.
          */
-        static inline Radian ATan2 (Real fY, Real fX) { return Radian(atan2(fY,fX)); }
+        static inline Radian ATan2 (Real fY, Real fX) { return Radian(std::atan2(fY,fX)); }
 
         /** Ceiling function
             Returns the smallest following integer. (example: Ceil(1.1) = 2)
@@ -303,7 +303,7 @@ namespace Ogre
             @param fValue
                 The value to round up to the nearest integer.
          */
-        static inline Real Ceil (Real fValue) { return Real(ceil(fValue)); }
+        static inline Real Ceil (Real fValue) { return std::ceil(fValue); }
         static inline bool isNaN(Real f)
         {
             // std::isnan() is C99, not supported by all compilers
@@ -319,7 +319,7 @@ namespace Ogre
                 calculation - faster but less accurate.
         */
         static inline Real Cos (const Radian& fValue, bool useTables = false) {
-            return (!useTables) ? Real(cos(fValue.valueRadians())) : SinTable(fValue.valueRadians() + HALF_PI);
+            return (!useTables) ? std::cos(fValue.valueRadians()) : SinTable(fValue.valueRadians() + HALF_PI);
         }
         /** Cosine function.
             @param fValue
@@ -329,10 +329,10 @@ namespace Ogre
                 calculation - faster but less accurate.
         */
         static inline Real Cos (Real fValue, bool useTables = false) {
-            return (!useTables) ? Real(cos(fValue)) : SinTable(fValue + HALF_PI);
+            return (!useTables) ? std::cos(fValue) : SinTable(fValue + HALF_PI);
         }
 
-        static inline Real Exp (Real fValue) { return Real(exp(fValue)); }
+        static inline Real Exp (Real fValue) { return std::exp(fValue); }
 
         /** Floor function
             Returns the largest previous integer. (example: Floor(1.9) = 1)
@@ -340,18 +340,18 @@ namespace Ogre
             @param fValue
                 The value to round down to the nearest integer.
          */
-        static inline Real Floor (Real fValue) { return Real(floor(fValue)); }
+        static inline Real Floor (Real fValue) { return std::floor(fValue); }
 
-        static inline Real Log (Real fValue) { return Real(log(fValue)); }
+        static inline Real Log (Real fValue) { return std::log(fValue); }
 
         /// Stored value of log(2) for frequent use
         static const Real LOG2;
 
-        static inline Real Log2 (Real fValue) { return Real(log(fValue)/LOG2); }
+        static inline Real Log2 (Real fValue) { return std::log(fValue)/LOG2; }
 
-        static inline Real LogN (Real base, Real fValue) { return Real(log(fValue)/log(base)); }
+        static inline Real LogN (Real base, Real fValue) { return std::log(fValue)/std::log(base); }
 
-        static inline Real Pow (Real fBase, Real fExponent) { return Real(pow(fBase,fExponent)); }
+        static inline Real Pow (Real fBase, Real fExponent) { return std::pow(fBase,fExponent); }
 
         static Real Sign (Real fValue);
         static inline Radian Sign ( const Radian& rValue )
@@ -386,7 +386,7 @@ namespace Ogre
                 calculation - faster but less accurate.
         */
         static inline Real Sin (const Radian& fValue, bool useTables = false) {
-            return (!useTables) ? Real(sin(fValue.valueRadians())) : SinTable(fValue.valueRadians());
+            return (!useTables) ? std::sin(fValue.valueRadians()) : SinTable(fValue.valueRadians());
         }
         /** Sine function.
             @param fValue
@@ -396,7 +396,7 @@ namespace Ogre
                 calculation - faster but less accurate.
         */
         static inline Real Sin (Real fValue, bool useTables = false) {
-            return (!useTables) ? Real(sin(fValue)) : SinTable(fValue);
+            return (!useTables) ? std::sin(fValue) : SinTable(fValue);
         }
 
         /** Squared function.
@@ -409,7 +409,7 @@ namespace Ogre
             @param fValue
                 The value whose square root will be calculated.
          */
-        static inline Real Sqrt (Real fValue) { return Real(sqrt(fValue)); }
+        static inline Real Sqrt (Real fValue) { return std::sqrt(fValue); }
 
         /** Square root function.
             @param fValue
@@ -417,7 +417,7 @@ namespace Ogre
             @return
                 The square root of the angle in radians.
          */
-        static inline Radian Sqrt (const Radian& fValue) { return Radian(sqrt(fValue.valueRadians())); }
+        static inline Radian Sqrt (const Radian& fValue) { return Radian(std::sqrt(fValue.valueRadians())); }
 
         /** Square root function.
             @param fValue
@@ -425,7 +425,7 @@ namespace Ogre
             @return
                 The square root of the angle in degrees.
          */
-        static inline Degree Sqrt (const Degree& fValue) { return Degree(sqrt(fValue.valueDegrees())); }
+        static inline Degree Sqrt (const Degree& fValue) { return Degree(std::sqrt(fValue.valueDegrees())); }
 
         /** Inverse square root i.e. 1 / Sqrt(x), good for vector
             normalisation.
@@ -472,7 +472,7 @@ namespace Ogre
                 calculation - faster but less accurate.
         */
         static inline Real Tan (const Radian& fValue, bool useTables = false) {
-            return (!useTables) ? Real(tan(fValue.valueRadians())) : TanTable(fValue.valueRadians());
+            return (!useTables) ? std::tan(fValue.valueRadians()) : TanTable(fValue.valueRadians());
         }
         /** Tangent function.
             @param fValue
@@ -482,7 +482,7 @@ namespace Ogre
                 calculation - faster but less accurate.
         */
         static inline Real Tan (Real fValue, bool useTables = false) {
-            return (!useTables) ? Real(tan(fValue)) : TanTable(fValue);
+            return (!useTables) ? std::tan(fValue) : TanTable(fValue);
         }
 
         static inline Real DegreesToRadians(Real degrees) { return degrees * fDeg2Rad; }

OgreMain/src/OgreAnimation.cpp
@@ -644,7 +644,7 @@ namespace Ogre {
         Real totalAnimationLength = mLength;
 
         if( timePos > totalAnimationLength && totalAnimationLength > 0.0f )
-            timePos = fmod( timePos, totalAnimationLength );
+            timePos = std::fmod( timePos, totalAnimationLength );
 
         // Search for global index
         KeyFrameTimeList::iterator it =

OgreMain/src/OgreAnimationTrack.cpp
@@ -98,10 +98,10 @@ namespace Ogre {
         {
             // Wrap time
             Real totalAnimationLength = mParent->getLength();
-            assert(totalAnimationLength > 0.0f && "Invalid animation length!");
+            OgreAssertDbg(totalAnimationLength > 0.0f, "Invalid animation length!");
 
             if( timePos > totalAnimationLength && totalAnimationLength > 0.0f )
-                timePos = fmod( timePos, totalAnimationLength );
+                timePos = std::fmod( timePos, totalAnimationLength );
 
             // No global keyframe index, need to search with local keyframes.
             KeyFrame timeKey(0, timePos);

OgreMain/src/OgreAutoParamDataSource.cpp
@@ -908,28 +908,28 @@ namespace Ogre {
     //-----------------------------------------------------------------------------
     Real AutoParamDataSource::getTime_0_X(Real x) const
     {
-        return fmod(this->getTime(), x);
+        return std::fmod(this->getTime(), x);
     }
     //-----------------------------------------------------------------------------
     Real AutoParamDataSource::getCosTime_0_X(Real x) const
     { 
-        return cos(this->getTime_0_X(x)); 
+        return std::cos(this->getTime_0_X(x));
     }
     //-----------------------------------------------------------------------------
     Real AutoParamDataSource::getSinTime_0_X(Real x) const
     { 
-        return sin(this->getTime_0_X(x)); 
+        return std::sin(this->getTime_0_X(x));
     }
     //-----------------------------------------------------------------------------
     Real AutoParamDataSource::getTanTime_0_X(Real x) const
     { 
-        return tan(this->getTime_0_X(x)); 
+        return std::tan(this->getTime_0_X(x));
     }
     //-----------------------------------------------------------------------------
     Vector4 AutoParamDataSource::getTime_0_X_packed(Real x) const
     {
         Real t = this->getTime_0_X(x);
-        return Vector4(t, sin(t), cos(t), tan(t));
+        return Vector4(t, std::sin(t), std::cos(t), std::tan(t));
     }
     //-----------------------------------------------------------------------------
     Real AutoParamDataSource::getTime_0_1(Real x) const
@@ -939,23 +939,23 @@ namespace Ogre {
     //-----------------------------------------------------------------------------
     Real AutoParamDataSource::getCosTime_0_1(Real x) const
     { 
-        return cos(this->getTime_0_1(x)); 
+        return std::cos(this->getTime_0_1(x));
     }
     //-----------------------------------------------------------------------------
     Real AutoParamDataSource::getSinTime_0_1(Real x) const
     { 
-        return sin(this->getTime_0_1(x)); 
+        return std::sin(this->getTime_0_1(x));
     }
     //-----------------------------------------------------------------------------
     Real AutoParamDataSource::getTanTime_0_1(Real x) const
     { 
-        return tan(this->getTime_0_1(x)); 
+        return std::tan(this->getTime_0_1(x));
     }
     //-----------------------------------------------------------------------------
     Vector4 AutoParamDataSource::getTime_0_1_packed(Real x) const
     {
         Real t = this->getTime_0_1(x);
-        return Vector4(t, sin(t), cos(t), tan(t));
+        return Vector4(t, std::sin(t), std::cos(t), std::tan(t));
     }
     //-----------------------------------------------------------------------------
     Real AutoParamDataSource::getTime_0_2Pi(Real x) const
@@ -965,23 +965,23 @@ namespace Ogre {
     //-----------------------------------------------------------------------------
     Real AutoParamDataSource::getCosTime_0_2Pi(Real x) const
     { 
-        return cos(this->getTime_0_2Pi(x)); 
+        return std::cos(this->getTime_0_2Pi(x));
     }
     //-----------------------------------------------------------------------------
     Real AutoParamDataSource::getSinTime_0_2Pi(Real x) const
     { 
-        return sin(this->getTime_0_2Pi(x)); 
+        return std::sin(this->getTime_0_2Pi(x));
     }
     //-----------------------------------------------------------------------------
     Real AutoParamDataSource::getTanTime_0_2Pi(Real x) const
     { 
-        return tan(this->getTime_0_2Pi(x)); 
+        return std::tan(this->getTime_0_2Pi(x));
     }
     //-----------------------------------------------------------------------------
     Vector4 AutoParamDataSource::getTime_0_2Pi_packed(Real x) const
     {
         Real t = this->getTime_0_2Pi(x);
-        return Vector4(t, sin(t), cos(t), tan(t));
+        return Vector4(t, std::sin(t), std::cos(t), std::tan(t));
     }
     //-----------------------------------------------------------------------------
     Real AutoParamDataSource::getFrameTime(void) const

OgreMain/src/OgreMath.cpp
@@ -37,7 +37,7 @@ namespace Ogre
     const Real Math::HALF_PI = Real( 0.5 * PI );
     const Real Math::fDeg2Rad = PI / Real(180.0);
     const Real Math::fRad2Deg = Real(180.0) / PI;
-    const Real Math::LOG2 = log(Real(2.0));
+    const Real Math::LOG2 = std::log(Real(2.0));
 
     int Math::mTrigTableSize;
    Math::AngleUnit Math::msAngleUnit;
@@ -78,9 +78,9 @@ namespace Ogre
         Real angle;
         for (int i = 0; i < mTrigTableSize; ++i)
         {
-            angle = Math::TWO_PI * i / mTrigTableSize;
-            mSinTable[i] = sin(angle);
-            mTanTable[i] = tan(angle);
+            angle = Math::TWO_PI * i / Real(mTrigTableSize);
+            mSinTable[i] = std::sin(angle);
+            mTanTable[i] = std::tan(angle);
         }
     }
     //-----------------------------------------------------------------------   

OgreMain/src/OgrePolygon.cpp
@@ -243,7 +243,7 @@ namespace Ogre
             else
             {
                 Real costheta = v1.dotProduct(v2) / (len1 * len2);
-                anglesum += acos(costheta);
+                anglesum += std::acos(costheta);
             }
         }
 

commit 2f6cb63b8f5e545228a6eedeb1e15e83e6c97b04

Main: fixup fmod wrap around behavior
broken in 02beb5d4ff16db423b0a9a19227df8a8f1c92d91

OgreMain/src/OgreAnimationState.cpp
@@ -80,6 +80,7 @@ namespace Ogre
             {
                 // Wrap
                 mTimePos = std::fmod(mTimePos, mLength);
+                if(mTimePos < 0) mTimePos += mLength;
             }
             else
             {

OgreMain/src/OgrePredefinedControllers.cpp
@@ -299,6 +299,7 @@ namespace Ogre
         Real output = 0;
         // For simplicity, factor input down to {0,1)
         input = std::fmod(input, Real(1));
+        if(input < 0) input += 1;
 
         // Calculate output in -1..1 range
         switch (mWaveType)