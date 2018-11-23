niu2x/ogre

commit 60692a4034091a7db1ad64d71b443d47cbcba03d

GLSupport: GLX - correctly handle windowMovedOrResized with parent win
and consequently remove superficial resize call in Bites

RenderSystems/GLSupport/src/GLX/OgreGLXWindow.cpp
@@ -697,29 +697,33 @@ namespace Ogre
         Display* xDisplay = mGLSupport->getXDisplay();
         XWindowAttributes windowAttrib;
 
+        Window parent, root, *children;
+        uint nChildren;
+
+        XQueryTree(xDisplay, mWindow, &root, &parent, &children, &nChildren);
+
+        if (children)
+            XFree(children);
+
+        XGetWindowAttributes(xDisplay, parent, &windowAttrib);
+
         if (mIsTopLevel && !mIsFullScreen)
         {
-            Window parent, root, *children;
-            uint nChildren;
-
-            XQueryTree(xDisplay, mWindow, &root, &parent, &children, &nChildren);
-
-            if (children)
-                XFree(children);
-
-            XGetWindowAttributes(xDisplay, parent, &windowAttrib);
-
             mLeft = windowAttrib.x;
             mTop  = windowAttrib.y;
         }
-
-        XGetWindowAttributes(xDisplay, mWindow, &windowAttrib);
 
         if (mWidth == (size_t)windowAttrib.width && mHeight == (size_t)windowAttrib.height)
             return;
 
         mWidth = windowAttrib.width;
         mHeight = windowAttrib.height;
+
+        if(!mIsTopLevel)
+        {
+            XResizeWindow(xDisplay, mWindow, mWidth, mHeight);
+            XFlush(xDisplay);
+        }
 
         for (ViewportList::iterator it = mViewportList.begin(); it != mViewportList.end(); ++it)
             (*it).second->_updateDimensions();

Components/Bites/src/OgreApplicationContext.cpp
@@ -792,7 +792,6 @@ void ApplicationContext::pollEvents()
                     continue;
 
                 Ogre::RenderWindow* win = it->render;
-                win->resize(event.window.data1, event.window.data2);
                 win->windowMovedOrResized();
                 windowResized(win);
             }

commit d07cbea3d02a44d7d3a2eb64f24734c9bee496c6

GLSupport: GLX - fix window size computation for top level windows
broken in 60692a4034091a7db1ad64d71b443d47cbcba03d

RenderSystems/GLSupport/src/GLX/OgreGLXWindow.cpp
@@ -709,8 +709,11 @@ namespace Ogre
 
         if (mIsTopLevel && !mIsFullScreen)
         {
+            // offset from window decorations
             mLeft = windowAttrib.x;
             mTop  = windowAttrib.y;
+            // w/ h of the actual renderwindow
+            XGetWindowAttributes(xDisplay, mWindow, &windowAttrib);
         }
 
         if (mWidth == (size_t)windowAttrib.width && mHeight == (size_t)windowAttrib.height)