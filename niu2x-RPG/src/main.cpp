bee654b7bc158ad73cc32197154f9ed86cd01b95
niu2x <niu2x@icloud.com>
2024-11-18 00:17:13 +0800

    niu2x-RPG

--- niu2x-RPG/src/main.cpp
+++ niu2x-RPG/src/main.cpp
@@ -2,12 +2,39 @@
 #include "OgreApplicationContext.h"
 #include <iostream>
 
+// class MyFrameListener : public Ogre::FrameListener
+// {
+// public:
+//     MyFrameListener():counter(0x160) {}
+//     ~MyFrameListener() {}
+
+//     bool frameStarted(const Ogre::FrameEvent& evt) override
+//     {
+//         return --counter > 0;
+//     }
+
+//     // bool frameRenderingQueued(const FrameEvent& evt) override
+//     // {
+//     //     (void)evt;
+//     //     return true;
+//     // }
+
+//     // bool frameEnded(const FrameEvent& evt) override
+//     // {
+//     //     (void)evt;
+//     //     return true;
+//     // }
+// private:
+//     size_t counter;
+// };
+
 int main()
 {
-
     OgreBites::ApplicationContext app;
+    // MyFrameListener l;
 
     app.initApp();
+    // app.getRoot()->addFrameListener(&l);
     app.getRoot()->startRendering();
     app.closeApp();
 
