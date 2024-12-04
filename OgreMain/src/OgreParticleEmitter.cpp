760a852e8674b67975b0ace84cac8c7f7171387e
xiaorong.niu <xiaorong.niu@happyelements.com>
2024-12-04 19:04:04 +0800

    doing

--- OgreMain/src/OgreParticleEmitter.cpp
+++ OgreMain/src/OgreParticleEmitter.cpp
@@ -25,9 +25,8 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE.
 -----------------------------------------------------------------------------
 */
-#include "OgreStableHeaders.h"
-
 #include "OgreParticleEmitter.h"
+
 #include "OgreParticleEmitterFactory.h"
 #include "OgreParticleEmitterCommands.h"
 
