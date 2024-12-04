028b60646f805e10492f89ac0946fe3f5db8a9d5
xiaorong.niu <xiaorong.niu@happyelements.com>
2024-12-04 18:23:52 +0800

    doing

--- OgreMain/src/OgreDualQuaternion.cpp
+++ OgreMain/src/OgreDualQuaternion.cpp
@@ -55,9 +55,11 @@ THE SOFTWARE.
 
 */
 
-#include "OgreStableHeaders.h"
 #include "OgreDualQuaternion.h"
 
+#include "OgreMatrix4.h"
+#include "OgreQuaternion.h"
+#include "OgreVector.h"
 
 namespace Ogre {
 
