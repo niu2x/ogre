f3d889c1e43f62a16d5cb77998e180792f718793
xiaorong.niu <xiaorong.niu@happyelements.com>
2024-12-16 10:57:27 +0800

    fix glsl

--- Samples/Media/materials/programs/GLSL/ShowUVdir3D.glsl
+++ Samples/Media/materials/programs/GLSL/ShowUVdir3D.glsl
@@ -1,8 +1,8 @@
 varying vec4 oUv0;
-
+out vec4 fragColor;
 // Basic fragment program to display 3d uv
 void main()
 {
 	vec3 n = normalize(oUv0.xyz);
-	gl_FragColor = vec4(n.x, n.y, n.z, 1.0);
+	fragColor = vec4(n.x, n.y, n.z, 1.0);
 }
