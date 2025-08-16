b32371d9fa15890d311ecc9ad6d44a0ccc9694c8
niu2x <niu2x@icloud.com>
2025-08-16 22:28:35 +0800

    Fix

--- main/include/hyue/String.h
+++ main/include/hyue/String.h
@@ -1,20 +1,31 @@
 #pragma once
 
 #include <string>
+#include <vector>
 
 namespace hyue {
 
 using String = std::string;
+using StringVector = std::vector<String>;
 
 struct StringUtils {
 
-    static String to_lower(const String &);
-    static String to_upper(const String &);
+    static String lower(const String &);
     static void lower(String *);
+
+    static String upper(const String &);
     static void upper(String *);
+
+    static void trim(String *, bool left=true, bool right=false);
+    static String trim(const String &, bool left=true, bool right=false);
 
     static bool starts_with(const String &str, const String &pattern, bool ignore_case=false);
     static bool ends_with(const String &str, const String &pattern, bool ignore_case=false);
+
+    // If compress_token argument is true, adjacent separators are merged together. Otherwise, every two separators delimit a token.
+    static StringVector split(const String& str, const String& delims, bool compress_token=false);
+
+    static String standardise_dir_path(const String& init);
 
 };
 
--- main/src/String.cpp
+++ main/src/String.cpp
@@ -1,41 +1,44 @@
 #include <hyue/String.h>
 
+#include <boost/algorithm/string.hpp>
+
 #include <algorithm>
 #include <cstring>
+
 
 #define strnicmp strncasecmp
 
 namespace hyue {
 
-String StringUtils::to_lower(const String &str) {
-    String copy = str;
-    lower(&copy);
-    return copy;
+String StringUtils::lower(const String &str) {
+    return boost::to_lower_copy(str);
 }
 
-String StringUtils::to_upper(const String &str) {
-    String copy = str;
-    upper(&copy);
-    return copy;
+String StringUtils::upper(const String &str) {
+    return boost::to_upper_copy(str);
 }
 
 void StringUtils::lower(String *str) {
-    std::transform(
-        str->begin(),
-        str->end(),
-        str->begin(),
-        tolower);
+    boost::to_lower(*str);
 }
 
 void StringUtils::upper(String *str) {
-    std::transform(
-        str->begin(),
-        str->end(),
-        str->begin(),
-        toupper);
-
+    boost::to_upper(*str);
 }
 
+void StringUtils::trim(String *str, bool left, bool right) {
+    static const String delims = " \t\r\n";
+    if(right)
+        str->erase(str->find_last_not_of(delims)+1); // trim right
+    if(left)
+        str->erase(0, str->find_first_not_of(delims)); // trim left
+}
+
+String StringUtils::trim(const String &str, bool left, bool right) {
+    String copy = str;
+    trim(&copy, left, right);
+    return copy;
+}
 
 bool StringUtils::starts_with(const String &str, const String &pattern, bool ignore_case) {
     if (pattern.empty())
@@ -66,6 +69,21 @@ bool StringUtils::ends_with(const String &str, const String &pattern, bool ignor
     return strncmp(str.c_str() + offset, pattern.c_str(), pattern.size()) == 0;
 }
 
+StringVector StringUtils::split(const String& str, const String& delims, bool compress_token) {
+    StringVector split_vec;
+    boost::split(split_vec, str, boost::is_any_of(delims), boost::token_compress_on);
+    return split_vec;
+}
+
+
+String StringUtils::standardise_dir_path(const String& init)
+{
+    String path = init;
+    std::replace( path.begin(), path.end(), '\\', '/' );
+    if( path[path.length() - 1] != '/' )
+        path += '/';
+    return path;
+}
 
 
 
