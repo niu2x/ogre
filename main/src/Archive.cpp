61a4e397b15bb6548996faf229fecd3a70b5efb9
niu2x <niu2x@icloud.com>
2025-08-23 20:28:26 +0800

    fix

--- main/src/Archive.cpp
+++ main/src/Archive.cpp
@@ -1,5 +1,7 @@
 #include <hyue/Archive.h>
 
+#include <hyue/panic.h>
+
 namespace hyue {
 
 Archive::Archive(const String& name, const String& arch_type)
@@ -13,6 +15,18 @@ Archive::Archive(const String& name, const String& arch_type)
  */
 Archive::~Archive()
 {
+}
+
+DataStreamPtr Archive::create(const String&)
+{
+    panic("This archive does not support creation of files. Archive::create");
+    return nullptr;
+}
+
+//---------------------------------------------------------------------
+void Archive::remove(const String&)
+{
+    panic("This archive does not support removal of files. Archive::remove");
 }
 
 } // namespace hyue
