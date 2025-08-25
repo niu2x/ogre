e82e32b10eaef062b97e5650e77781bbc5ed5ba0
niu2x <niu2x@icloud.com>
2025-08-25 19:10:41 +0800

    fix

--- Makefile
+++ Makefile
@@ -1,4 +1,6 @@
 build:
+	cmake -S deps -B build_deps -G Ninja
+	cmake --build build_deps -j 3
 	cmake -S . -B build -G Ninja -DHYUE_BUILD_TEST=ON -DHYUE_BUILD_EXAMPLE=ON
 	cmake --build build -j 3
 
--- cmake/build_deps.cmake
+++ cmake/build_deps.cmake
@@ -4,10 +4,6 @@ ExternalProject_Add(
     libzip
     GIT_REPOSITORY https://github.com/nih-at/libzip
     GIT_TAG        v1.11.4
-    PREFIX         ${CMAKE_BINARY_DIR}/deps/libzip
+    PREFIX         ${CMAKE_BINARY_DIR}/libzip
     CMAKE_ARGS     -DCMAKE_INSTALL_PREFIX=<INSTALL_DIR>
 )
-
-add_dependencies(${HYUE_LIB} libzip)
-
-set(libzip_DIR ${CMAKE_BINARY_DIR}/deps/libzip/lib/cmake/libzip)
