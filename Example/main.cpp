903cebb66e6dbb95cb8167b7cf31d6200f56da03
niu2x <niu2x@icloud.com>
2025-08-25 19:55:17 +0800

    fix

--- Example/main.cpp
+++ Example/main.cpp
@@ -1,23 +1,48 @@
 #include <hyue/Root.h>
 #include <hyue/FileSystemArchiveFactory.h>
+#include <hyue/ZipArchiveFactory.h>
 #include <hyue/math.h>
 
 int main()
 {
-    hyue::FileSystemArchiveFactory factory;
-    auto archive = factory.create_instance("./Docs");
-    archive->load();
+    {
+        hyue::FileSystemArchiveFactory factory;
+        auto archive = factory.create_instance("./Docsx");
+        archive->load();
 
-    auto file_infos = archive->list_file_info(true);
-    for(auto &item: file_infos) {
-        printf("\n"); 
-        printf("filename: %s\n", item.filename.c_str());
-        printf("dir: %s\n", item.dir.c_str());
-        printf("basename: %s\n", item.basename.c_str());
+        auto file_infos = archive->list_file_info(true);
+        for (auto& item : file_infos) {
+            printf("\n");
+            printf("filename: %s\n", item.filename.c_str());
+            printf("dir: %s\n", item.dir.c_str());
+            printf("basename: %s\n", item.basename.c_str());
+        }
+
+        archive->unload();
+        factory.destroy_instance(archive);
     }
 
-    archive->unload();
-    factory.destroy_instance(archive);
+    {
+
+        hyue::ZipArchiveFactory factory;
+        auto archive = factory.create_instance("test.zip");
+        archive->load();
+
+        auto file_infos = archive->list_file_info(true);
+        for (auto& item : file_infos) {
+            printf("\n");
+            printf("filename: %s\n", item.filename.c_str());
+            printf("dir: %s\n", item.dir.c_str());
+            printf("basename: %s\n", item.basename.c_str());
+        }
+
+        printf("%d\n", archive->exists("test"));
+        printf("%d\n", archive->exists("test/test.x"));
+        printf("%d\n", archive->exists("test/test.txt"));
+
+        archive->unload();
+        factory.destroy_instance(archive);
+    }
 
     return 0;
 }
